ee13ec1c5d77f1a7d098893b3e12261c3fc27f8d
Programing Hero <sajalb1899@gmail.com>
2023-08-21 04:03:38 +0600

    Coupon code added

--- js/script.js
+++ js/script.js
@@ -19,19 +19,59 @@ function getTitle(input3,input4){
 
     const clickProduct = document.getElementById(input4);
     const p = document.createElement('p');
-    p.innerHTML=`${inputFelidString}`
+    p.classList.add('mb-3')
+    const count = clickProduct.childElementCount;
+    p.innerHTML=`${count + 1} ${inputFelidString}`
     clickProduct.appendChild(p);
-    
-
-    console.log(p);
 }
 
+function disableButton(number){
+    const price = document.getElementById(number);
+    const priceString = price.innerText;
+    const priceNumber = parseFloat(priceString);
+    const makePurchase = document.getElementById("make-Purchase");
+    if(priceNumber > 0){
+        makePurchase.removeAttribute('disabled')
+
+    }
+}
+
+function applyButton(number1){
+    const price = document.getElementById(number1);
+    const priceString = price.innerText;
+    const priceNumber = parseFloat(priceString);
+    const makePurchase = document.getElementById("btn-apply");
+    if(priceNumber > 199){
+        makePurchase.removeAttribute('disabled');
+
+    }
+}
+
+
+
+document.getElementById('btn-apply').addEventListener('click', function(){
+    const inputCoupon = document.getElementById('coupon-input');
+    const inputCouponValue = inputCoupon.value;
+    if(inputCouponValue === 'SELL200'){
+        alert('20% discount')
+    }
+    else{
+        alert('Please Enter Valid Coupon')
+    }
+})
+
+
+
 const mainPrice = 'total-price';
+const productHistory = 'product-history';
 document.getElementById("product-1").addEventListener('click', function(){
     const getProduct1Price= getPrice('product-1-price', mainPrice);
-    const productTitle = getTitle('product-1-title','product-history')
+    const productTitle = getTitle('product-1-title',productHistory);
+    const disableBtn = disableButton('total-price');
+    const disApplyButton = applyButton('total-price');
 })
 
 document.getElementById("product-2").addEventListener('click', function(){
     const getProduct1Price= getPrice('product-2-price', mainPrice);
+    const productTitle = getTitle('product-2-title',productHistory);
 })
